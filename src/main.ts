3a2a1c72742ed332b7ba1bb21a3f45918d44eea7
Jeffrey Lym <jeffreyalym@gmail.com>
2018-08-13 22:22:56 -0700

    Read a file and print its value

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("Hello world");
+import * as fs from "fs";
+import * as util from "util";
+
+const readFile = util.promisify(fs.readFile);
+
+const main = async () => {
+    try
+    {
+        const value = await readFile("./file.txt", { encoding: "utf8" });
+        console.log(value);
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
+main();
